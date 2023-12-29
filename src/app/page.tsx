ee14c29d4a5993d01782666a4d40069ffd7aae3e
pubudulakshani <pubudu.creativehub@gmail.com>
2023-12-29 13:27:09 +0530

    front page text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            cdcdcd
+        <main className="flex flex-col items-center justify-between">
+            <h3 className='py-12'>
+                This is the client side (Frontend) for the Employee CRUD Operations.
+                As per the guidelines given in the assessment, the pages are given below.
+            </h3>
+                <li>
+                    <Link href={'/employee/list'} className='text-blue-600 underline'> Employee list </Link>
+                </li>
+                <li>
+                    <Link href={'/employee/add'} className='text-blue-600 underline'> Add New Employee </Link>
+                </li>            
         </main>
     )
 }
